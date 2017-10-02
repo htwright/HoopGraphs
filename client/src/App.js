76e29385757ce3d4d406782e703887ce960c5698
htwright <harrisontwright@gmail.com>
2017-10-02 12:41:14 -0500

    drawing chart via component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Graph from './graph/graph';
 
 class App extends Component {
 
@@ -32,6 +33,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Graph />
       </div>
     );
   }
