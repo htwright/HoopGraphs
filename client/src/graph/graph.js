5c8af1fa13ebebdc4ea90a1d4be30591832d8b0f
htwright <harrisontwright@gmail.com>
2017-10-02 12:41:36 -0500

    missing file

---
+++ client/src/graph/graph.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+const d3 = require('react-d3');
+const LineChart = d3.LineChart;
+
+
+export default class Graph extends Component {
+
+  render(){
+    const lineData = [
+      {
+        name: 'series1',
+        values: [ { x: 0, y: 20 }, { x: 24, y: 10 } ]
+      },
+      {
+        name: 'series2',
+        values: [ { x: 70, y: 82 }, { x: 76, y: 82 } ]
+      }
+    ];
+
+    return (<LineChart legend={true} data={lineData} width={500} height={300} title="chart"/>);
+  }
+
+}
